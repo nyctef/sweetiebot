a9d2edad3bad229cd66192f438e7979106a2d443
Nyctef <nyctef@nyctef.com>
2014-01-12 20:11:14 +0000

    more extraction, add MUCJabberBot.py

--- sweetiebot.py
+++ sweetiebot.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 
-from jabberbot import JabberBot, botcmd
+from jabberbot import botcmd
 from datetime import datetime
 import re
 import urllib
@@ -14,104 +14,10 @@ import difflib
 import redis
 import sys
 import logging
-from functools import wraps
 import requests
 import utils
-
-def logerrors(func):
-    @wraps(func)
-    def logged(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except Exception:
-            logging.exception('Error in '+func.__name__)
-            return "My code is problematic :sweetieoops:"
-    return logged
-
-
-class MUCJabberBot(JabberBot):
-
-    flood_protection = 0
-    flood_delay = 5
-    PING_FREQUENCY = 60
-
-    def __init__(self, nickname, *args, **kwargs):
-        ''' Initialize variables. '''
-
-        # answer only direct messages or not?
-        self.nickname = nickname
-        self.only_direct = kwargs.get('only_direct', False)
-
-        try:
-            del kwargs['only_direct']
-        except KeyError:
-            pass
-
-        # initialize jabberbot
-        super(MUCJabberBot, self).__init__(*args, **kwargs)
-
-        # create a regex to check if a message is a direct message
-        user, domain = str(self.jid).split('@')
-        self.direct_message_re = re.compile('^%s(@%s)?[^\w]? '
-                                            % (user, domain))
-
-    def callback_message(self, conn, mess):
-        ''' Changes the behaviour of the JabberBot in order to allow
-        it to answer direct messages. This is used often when it is
-        connected in MUCs (multiple users chatroom). '''
-        # fuck you unicode
-        message = mess.getBody()
-        props = mess.getProperties()
-        jid = mess.getFrom()
-        try:
-            if self.direct_message_re.match(message):
-                mess.setBody(' '.join(message.split(' ', 1)[1:]))
-                super(MUCJabberBot, self).callback_message(conn, mess)
-        except TypeError:
-            return
-        if not message:
-            return
-        if xmpp.NS_DELAY in props:
-            return
-        if self.jid.bareMatch(jid):
-            return
-        if utils.is_ping(self.nickname, message):
-            np_message = self.fix_ping(message)
-        else:
-            np_message = message
-        if ' ' in np_message:
-            command, args = np_message.split(' ', 1)
-        else:
-            command, args = np_message, ''
-
-        cmd = command.lower()
-        if cmd in self.commands:
-            if utils.is_ping(self.nickname, message):
-                mess.setBody(np_message)
-                super(MUCJabberBot, self).callback_message(conn, mess)
-            else:
-                return
-        else:
-            reply = self.unknown_command(mess, cmd, args)
-            if reply is None:
-                return
-            if reply:
-                self.send_simple_reply(mess, reply)
-        return
-
-    def load_commands_from(self, target):
-        import inspect
-        for name, value in inspect.getmembers(target, inspect.ismethod):
-            if getattr(value, '_jabberbot_command', False):
-                name = getattr(value, '_jabberbot_command_name')
-                self.log.info('Registered command: %s' % name)
-                self.commands[name] = value
-
-    def fix_ping(self, message):
-        message = message.replace(self.nickname+": ", "")
-        message = message.replace(self.nickname.lower()+": ", "")
-        return message
-
+from utils import logerrors
+from MUCJabberBot import MUCJabberBot
 
 class Sweetiebot():
     kick_owl_delay = 7200
--- utils.py
+++ utils.py
@@ -6,3 +6,14 @@ def is_ping(nickname, message):
         return False
 
 
+def logerrors(func):
+    from functools import wraps
+    @wraps(func)
+    def logged(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except Exception:
+            logging.exception('Error in '+func.__name__)
+            return "My code is problematic :sweetieoops:"
+    return logged
+
