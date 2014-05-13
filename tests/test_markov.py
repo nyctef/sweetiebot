bc849c05b2eb81d01d919c71cfaaebe797545aa3
Nyctef <nyctef@nyctef.com>
2014-05-13 21:49:08 +0100

    this should still be using FakeRedis

--- tests/test_markov.py
+++ tests/test_markov.py
@@ -4,8 +4,6 @@ import os
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir)
 
-import redis
-
 from modules import SweetieMarkov, FakeRedis
 
 def read_dump():
@@ -15,7 +13,7 @@ def read_dump():
 
 if __name__ == '__main__':
     lines = read_dump()
-    markov = SweetieMarkov(redis.Redis('localhost'), '../data/banned_keywords.txt',
+    markov = SweetieMarkov(FakeRedis(), '../data/banned_keywords.txt',
                       '../data/preferred_keywords.txt', '../data/swap_words.txt')
     #for line in lines:
         #markov.store_message(line)
