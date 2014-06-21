86d868cdbdb4636f70094a8e951aa1cc94fbc234
Nyctef <nyctef@nyctef.com>
2014-06-21 23:55:15 +0000

    remove old import

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,7 +3,6 @@ from SweetieAdmin import SweetieAdmin
 from SweetieChat import SweetieChat
 from SweetieLookup import SweetieLookup
 from SweetieMQ import SweetieMQ
-from SweetieRedis import SweetieRedis
 from MUCJabberBot import MUCJabberBot, RestartException
 from FakeRedis import FakeRedis
 from SweetieRoulette import SweetieRoulette
