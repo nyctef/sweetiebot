1e10f839f8b4724df31a213bb658ad934c352ff5
Nyctef <nyctef@nyctef.com>
2017-04-30 02:08:21 +0100

    pretty up the script output a bit

--- run-bot-and-watch.sh
+++ run-bot-and-watch.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
 # start sweetiewatch
-echo "starting sweetiewatch"
+echo "Starting sweetiewatch..."
+echo '********************************************************************************' >> data/sweetiewatch.log
+echo "Starting log at" `date '+%Y-%m-%d %H:%M:%S'` >> data/sweetiewatch.log
+echo '********************************************************************************' >> data/sweetiewatch.log
 python sweetiewatch.py >> data/sweetiewatch.log 2>&1 &
 # We wait a few seconds before starting sweetiebot to make sure the jabber server picks it up as running second.
 # Annoyingly, ejabberd seems to throw away all our carefully-set resource priorities and just delivers
 # PMs to whichever client connected most recently, so make sure that's sweetiebot.
-echo "pausing"
+echo "... done. Pausing"
 sleep 5
-echo "starting sweetiebot"
+echo "Starting sweetiebot..."
+echo '********************************************************************************' >> data/sweetiebot.log
+echo "Starting log at" `date '+%Y-%m-%d %H:%M:%S'` >> data/sweetiebot.log
+echo '********************************************************************************' >> data/sweetiebot.log
 python sweetiebot.py >> data/sweetiebot.log 2>&1 &
-echo "sweetiebot started"
+echo "... done. Waiting for sweetiebot to quit..."
 # wait for bot to exit before exiting script
 wait %1
-echo "sweetiebot ended, cleaning up"
+echo "Sweetiebot ended, cleaning up remaining jobs..."
 # kill sweetiewatch instead of leaving it lying around
 kill $(jobs -p)
-echo "remaining jobs killed"
+echo "... done. Exiting"
