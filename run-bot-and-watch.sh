18e75e45a947e64c9732b59f3de9f48aa6ac2752
Nyctef <nyctef@nyctef.com>
2017-05-13 16:42:10 +0100

    put run-bot-and-watch into a loop
    combined with the crash-on-disconnect behaviour the bot now has, this
    should make it handle disconnects a lot more cleanly

--- run-bot-and-watch.sh
+++ run-bot-and-watch.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# start sweetiewatch
+while true
+do
+
 echo "Starting sweetiewatch..."
 echo '********************************************************************************' >> data/sweetiewatch.log
 echo "Starting log at" `date '+%Y-%m-%d %H:%M:%S'` >> data/sweetiewatch.log
@@ -11,13 +13,19 @@ python sweetiewatch.py >> data/sweetiewatch.log 2>&1 &
 # PMs to whichever client connected most recently, so make sure that's sweetiebot.
 echo "... done. Pausing"
 sleep 5
+
 echo "Starting sweetiebot..."
 echo '********************************************************************************' >> data/sweetiebot.log
 echo "Starting log at" `date '+%Y-%m-%d %H:%M:%S'` >> data/sweetiebot.log
 echo '********************************************************************************' >> data/sweetiebot.log
 python sweetiebot.py 2>&1 | tee -a data/sweetiebot.log
+
 # wait for bot to exit before exiting script
 echo "...Sweetiebot ended, cleaning up remaining jobs..."
 # kill sweetiewatch instead of leaving it lying around
 kill $(jobs -p)
-echo "... done. Exiting"
+echo "... done. Pausing"
+sleep 10
+echo "and looping..."
+
+done
