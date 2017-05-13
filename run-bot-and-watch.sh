1ae2f2599a2ba3d827e0f076da560e6af10cbe25
Nyctef <nyctef@nyctef.com>
2017-05-13 15:38:13 +0100

    tee output to console so we have something to look at here

--- run-bot-and-watch.sh
+++ run-bot-and-watch.sh
@@ -15,7 +15,7 @@ echo "Starting sweetiebot..."
 echo '********************************************************************************' >> data/sweetiebot.log
 echo "Starting log at" `date '+%Y-%m-%d %H:%M:%S'` >> data/sweetiebot.log
 echo '********************************************************************************' >> data/sweetiebot.log
-python sweetiebot.py >> data/sweetiebot.log 2>&1 &
+python sweetiebot.py 2>&1 | tee -a data/sweetiebot.log &
 echo "... done. Waiting for sweetiebot to quit..."
 # wait for bot to exit before exiting script
 wait %1
