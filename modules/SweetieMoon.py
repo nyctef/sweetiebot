2eea87505199a6b82b8d001fd6c7470c5a18f46d
Nyctef <nyctef@nyctef.com>
2017-09-03 15:35:27 +0100

    Inline .format() logic into phase_description
    to remove some awkwardness in the phrasing / add a bit of variety

--- modules/SweetieMoon.py
+++ modules/SweetieMoon.py
@@ -10,18 +10,18 @@ class SweetieMoon(object):
         if bot:
             bot.load_commands_from(self)
 
-    def phase_description(self, phase):
+    def phase_description(self, phase, percentage):
         descriptions = {
-                'new moon': "hiding. Spoopiness levels are at a minimum and most incantations are safe to perform.",
-                'waxing crescent': "growing.",
-                'first quarter': "growing. Preparations are not necessary yet, but be prepared.",
-                'waxing gibbous': "growing, and almost full.",
-                'full moon': "fully operational. Please be careful when leaving your designated shelter after dark.",
-                'waning gibbous': "waning.",
-                'last quarter': "waning. Normalcy levels are within tolerances.",
-                'waning crescent': "continuing to wane.",
+                'new moon': "Currently, the moon is hiding. Spoopiness levels are at a minimum and most incantations are safe to perform.",
+                'waxing crescent': "Currently, the moon is {percentage} full and growing.",
+                'first quarter': "Currently, the moon is half full and growing. Preparations are not necessary yet, but be prepared.",
+                'waxing gibbous': "Currently, the moon is {percentage} full and growing. It's almost full!",
+                'full moon': "The moon is full and fully operational. Please be careful when leaving your designated shelter after dark.",
+                'waning gibbous': "The moon is currently {percentage} full and waning.",
+                'last quarter': "Currently the moon is {percentage} full and waning. Normalcy levels are now within acceptable tolerances.",
+                'waning crescent': "The moon is {percentage} full and continuing to wane.",
                 }
-        return descriptions.get(phase.lower(), 'unknown. This is worrying.')
+        return descriptions.get(phase.lower(), 'The moon phase is unknown. This is worrying.').format(percentage=percentage)
 
     def phase_percentage(self, phase):
         descriptions = {
@@ -50,12 +50,11 @@ class SweetieMoon(object):
         if not phase:
             phase = result['closestphase']['phase']
 
-        description = self.phase_description(phase)
         percentage = result.get('fracillum', None)
         if not percentage:
             percentage = self.phase_percentage(phase)
 
-        return 'Currently, the moon is {} full and {}'.format(percentage, description)
+        return self.phase_description(phase, percentage)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
