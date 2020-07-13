eaf22a7e37fb43e9e1976baeeaef09f99602ca16
nyctef <nyctef@nyctef.com>
2020-07-13 13:58:57 +0100

    wip: extract more functions into TellStorageRedis

--- modules/SweetieTell.py
+++ modules/SweetieTell.py
@@ -11,12 +11,28 @@ class TellStorageRedis(object):
     def __init__(self, store):
         self.store = store
 
+    def enc(self, str):
+        return str.encode('utf-8')
+
+    def dec(self, bytes):
+        return bytes.decode('utf-8')
+
     def get_jid_from_nick(self, nick):
         result = self.store.get(f'jidfornick:{nick}')
-        if result: return result.decode('utf-8')
+        if result: return self.dec(result)
 
     def set_jid_for_nick(self, nick, jid):
         self.store.set(f'jidfornick:{nick}', jid)
+
+    def set_or_update_message(self, jid, senderjid, message):
+        self.store.hset(f'tell:{jid}', str(senderjid), message)
+
+    def get_existing_messages_by_sender(self, jid):
+        messages = self.store.hgetall(f'tell:{jid}')
+        return {self.dec(k):self.dec(v) for (k,v) in messages.items()}
+        
+    def clear_messages_for(self, jid):
+        self.store.delete(f'tell:{jid}')
 
 class SweetieTell(object):
     def __init__(self, bot, store):
@@ -25,12 +41,6 @@ class SweetieTell(object):
         self.bot = bot
         self.bot.load_commands_from(self)
         self.nicktojid = NickToJidTracker(self.bot, self.storage)
-
-    def enc(self, str):
-        return str.encode('utf-8')
-
-    def dec(self, bytes):
-        return bytes.decode('utf-8')
 
     @botcmd
     def tell(self, message):
@@ -57,34 +67,23 @@ class SweetieTell(object):
         if len(mess) > 1000:
             return 'Sorry, that message is too long (1000 char maximum)'
 
-        existing_messages = self._get_existing_messages_by_sender(sendee_jid)
-        existing_message = existing_messages.get(self.enc(str(sender_jid)), None)
+        existing_messages = self.storage.get_existing_messages_by_sender(sendee_jid)
+        existing_message = existing_messages.get(str(sender_jid), None)
         if existing_message is not None:
-            combined_message = self.dec(existing_message) + '\n' + mess
+            combined_message = existing_message + '\n' + mess
             if len(combined_message) > 1000:
                 return 'Sorry, that message is too long (1000 char maximum; you\'ve already used ~{})'.format(len(existing_message))
-            self._set_or_update_message(sendee_jid, sender_jid, combined_message)
+            self.storage.set_or_update_message(sendee_jid, sender_jid, combined_message)
             return 'Message received for {} (appended to previous message)'.format(sendee_jid)
         if sendee_nick and mess:
-            self._set_or_update_message(sendee_jid, sender_jid, '{} left you a message: {}'.format(sender_nick, mess))
+            self.storage.set_or_update_message(sendee_jid, sender_jid, '{} left you a message: {}'.format(sender_nick, mess))
             return 'Message received for {}'.format(sendee_jid)
 
-    def _key(self, jid):
-        return 'tell:{}'.format(str(jid))
-
     def get_messages_for(self, message):
-        key = self._key(message.user_jid)
-        messages = self.store.hvals(key)
-        messages = list(map(lambda x: x.decode('utf-8'), messages))
+        messages = self.storage.get_existing_messages_by_sender(message.user_jid).values()
         if len(messages):
-            self.store.delete(key)
+            self.storage.clear_messages_for(message.user_jid)
             return message.sender_nick + ", " + "\n".join(messages)
-
-    def _set_or_update_message(self, jid, senderjid, message):
-        self.store.hset(self._key(jid), str(senderjid), message)
-
-    def _get_existing_messages_by_sender(self, jid):
-        return self.store.hgetall(self._key(jid))
 
 class NickToJidTracker(object):
     def __init__(self, bot, storage):
