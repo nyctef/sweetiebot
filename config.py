0d863c323498b6cbe69887663427c06367895a80
nyctef <nyctef@nyctef.com>
2020-07-25 12:00:55 +0100

    start working on setting up azure logging

--- config.py
+++ config.py
@@ -14,15 +14,22 @@ port = getenv('SB_PORT', 5222)
 debug = getenv('SB_DEBUG', False)
 
 # Optional: API key for pushbullet notifications
+# TODO: remove
 pushbullet_api = getenv('PB_API_KEY', None)
 # Optional: Device ID for pushbullet notifications
+# TODO: remove
 pushbullet_device = getenv('PB_DEVICE_ID', None)
 
 # Optional: Twitter API for putting twitter feeds into chat
+# TODO: remove
 twitter_key = getenv('TWITTER_API_KEY', None)
 twitter_secret = getenv('TWITTER_API_SECRET', None)
 
 # A url for the redis instance to connect to in the form redis://[:password]@host:port/dbnum
+# TODO: remove in favour of pg
 redis_url = getenv('REDIS_DB_URL', 'redis://sbredis:6379/0')
-
+# Connection string for backing storage
 pg_conn_str = getenv('SB_PG_DB', None)
+
+# Azure Monitor app insights key for opencensus logging
+app_insights_key = getenv('SB_APPINSIGHTS_KEY', None)
